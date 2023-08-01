50a696927baef30d2fa69016b118e723c1865a64
Nikhil Dixit <nikhil09.dixit@gmail.com>
2023-08-01 18:00:56 +0530

    imageUpload

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API=axios.create({baseURL:'http://localhost:8000/'});
+const API=axios.create({baseURL:'https://hostel-backend-sigma.vercel.app/'});
 API.interceptors.request.use((req)=>{
     // var tok=JSON.parse(localStorage.getItem('profile')).token;
     // console.log(tok);
